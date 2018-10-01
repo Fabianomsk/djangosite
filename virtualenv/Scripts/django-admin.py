fd39345aa5390cdc461fcb4f6bc770ff5becc0d1
Fabianomsk <fabianomsk@gmail.com>
2018-09-30 23:15:13 -0300

    primeiro commit

---
+++ virtualenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\fabiano\documents\projetospython3\virtualenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
