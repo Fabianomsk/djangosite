122fabf22e45d7334b002f5f951eb0215bf042a6
Fabianomsk <fabianomsk@gmail.com>
2018-10-04 20:08:10 -0300

    atualizacao20181003

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
-    path('index/', include('polls.urls')),
-    path('admin/', admin.site.urls) 
+    path('admin/', admin.site.urls),    
 ]
